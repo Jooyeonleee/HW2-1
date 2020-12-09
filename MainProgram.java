ac76f34d7cfda34b146533050880a4200c5ddbd3
Yejilee <airel7@naver.com>
2020-12-09 17:00:23 +0900

    1610479 이예지

--- MainProgram.java
+++ MainProgram.java
@@ -33,6 +33,10 @@ public class MainProgram {
             System.out.println("[Student ID: "+studentID+"]");
             hw2_1612967();
         }
+        else if (studentID == 1610479){
+            System.out.println("[Student ID: "+studentID+"]");
+            hw2_1610479();
+        }
         else {
             System.out.println("To be developed...\n");
         }
@@ -111,5 +115,46 @@ public class MainProgram {
         else{
             return a;
         }
+    }
+    
+    public void hw2_1610479() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("1. Calculate max among two numbers");
+        System.out.println("2. Calculate factorial of integer x");
+
+        System.out.print("Enter menu number: ");
+        int menu = scan.nextInt();
+        int a,b;
+        if (menu == 1) {
+            System.out.println("Enter first number");
+            a = scan.nextInt();
+            System.out.println("Enter second number");
+            b = scan.nextInt();
+            System.out.println("Max number is: " +maxyeji(a,b));
+        }
+        else if (menu == 2) {
+            System.out.println("Enter integer number");
+            a = scan.nextInt();
+            System.out.println("Factorial is: " +facyeji(a));
+        }
+    }
+
+    public int maxyeji(int a, int b) {
+        int max;
+        if (a>=b) max = a;
+        else max = b;
+        return max;
+    }
+    public int facyeji(int fac) {
+        int num=fac;
+        if (fac==0) {
+        	return 1;
+        }
+        while(fac!=1){
+        	fac--;
+        	num=num*fac;
+        }
+        return num;
     }
 }
